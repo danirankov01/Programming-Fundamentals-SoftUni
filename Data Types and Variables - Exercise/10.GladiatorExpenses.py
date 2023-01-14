b6044b4ba49969a43d3d9ca9079cd0035288fb0f
danirankov01 <122024239+danirankov01@users.noreply.github.com>
2023-01-14 19:53:25 +0200

    Data Types Exercises

--- Data Types and Variables - Exercise/10.GladiatorExpenses.py
+++ Data Types and Variables - Exercise/10.GladiatorExpenses.py
@@ -1,24 +1,51 @@
-lostFights = int(input())
+# lostFights = int(input())
+# helmetPrice = float(input())
+# swordPrice = float(input())
+# shieldPrice = float(input())
+# armorPrice = float(input())
+
+# lost = 0
+# total = 0
+# shieldBreaks = 0
+# while(lostFights != 0):
+#     lost += 1
+#     if(lost % 2 == 0 and lost % 3 == 0):
+#         total += helmetPrice + swordPrice + shieldPrice
+#         shieldBreaks += 1
+#         if(shieldBreaks == 2):
+#             total += armorPrice
+#     else:
+#         if(lost % 2 == 0):
+#             total += helmetPrice
+#         if(lost % 3 == 0):
+#             total += swordPrice
+#     lostFights -= 1
+
+# print(f"Gladiator expenses: {total:.2f} aureus")
+
+
+
+lostFightCount = int(input())
+
 helmetPrice = float(input())
 swordPrice = float(input())
 shieldPrice = float(input())
 armorPrice = float(input())
 
-lost = 0
+fightsCount = 0
 total = 0
-shieldBreaks = 0
-while(lostFights != 0):
-    lost += 1
-    if(lost % 2 == 0 and lost % 3 == 0):
-        total += helmetPrice + swordPrice + shieldPrice
-        shieldBreaks += 1
-        if(shieldBreaks == 2):
-            total += armorPrice
-    else:
-        if(lost % 2 == 0):
-            total += helmetPrice
-        if(lost % 3 == 0):
-            total += swordPrice
-    lostFights -= 1
+shieldCounter = 0
+while(lostFightCount > 0):
+    fightsCount += 1
+    if(fightsCount % 2 == 0):
+        total += helmetPrice
+    if(fightsCount % 3 == 0):
+        total += swordPrice
+        if(fightsCount % 2 == 0):
+            total += shieldPrice
+            shieldCounter += 1
+            if(shieldCounter % 2 == 0):
+                total += armorPrice
+    lostFightCount -= 1
 
-print(f"Gladiator expenses: {total:.2f} aureus")
+print(f"Gladiator expenses: {total} aureus")
