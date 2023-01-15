b737265af1f5a7848d99a4bf5e9ce449f0c85fce
danirankov01 <122024239+danirankov01@users.noreply.github.com>
2023-01-16 01:29:43 +0200

    List Basics - More Exercise

--- Lists Basics - More Exercises/04.JosephusPermutation.py
+++ Lists Basics - More Exercises/04.JosephusPermutation.py
@@ -3,8 +3,21 @@ movingPositions = int(input())
 listNumbers = []
 result = []
 
+counter = 0
+index = 0
+
 for i in inpt:
     listNumbers.append(int(i))
 
-for i in range(len(listNumbers)):
-    result.append(listNumbers[movingPositions - 1])
+while(len(listNumbers) != 0):
+    counter += 1
+    if(counter % movingPositions == 0):
+        result.append(listNumbers[index])
+        listNumbers.pop(index)
+    elif(counter % movingPositions != 0):
+        index += 1
+
+    if(index >= len(listNumbers)):
+        index = 0
+
+print(str(result).replace(' ', ''))
